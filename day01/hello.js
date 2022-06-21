4ea9aab59ac57f35a1eab989b19f4e2d76a2a82d
yihyunkong <mlzzh@naver.com4>
2022-06-21 10:08:05 +0900

    22.06.21 commit

--- day01/hello.js
+++ day01/hello.js
@@ -3,6 +3,7 @@ let colors = require('colors');
 console.log(colors.rainbow('Hello NodeJS 귀엽다 ><'));
 console.log(colors.green('Hello NodeJS 귀엽다 ><'));
 console.log(colors.red('Hello NodeJS 귀엽다 ><'));
-console.log(colors.yellow('git 연동 성공!!!'))
+console.log(colors.yellow('git 연동 성공!!!'));
+console.log(colors.yellow('git 연동 성공!!!'));
 
 // npm i nodemon -g >> 저장하면 명령어를 실행하지 않아도 바로 터미널에 보여준다.
